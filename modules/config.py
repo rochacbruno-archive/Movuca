43fce34afee6e5444e373fe952f50d6e59f9b491
Bruno Rocha <rochacbruno@gmail.com>
2011-11-13 04:47:31 -0200

    removed some files

--- modules/config.py
+++ modules/config.py
@@ -43,38 +43,3 @@ class Config(object):
         options = self.__getattribute__(table)[option]
         assert isinstance(options, list)
         return [(option.split(":")[0], str(self.T(option.split(":")[1]))) for option in options]
-
-        # database
-        #self.db_migrate = True
-        #self.db_pool_size = 10
-        #self.db_uri = "sqlite://config101.sqlite"
-        #self.db_uri = 'postgres://tutor:302010@localhost/movuca1'
-        # self.db_gaeuri = "google:datastore"
-        # self.db_migrate_enabled = True
-        # self.mail_server = 'logging'
-        # self.mail_sender = 'teste@teste.com'
-        # self.mail_login = 'teste:1234'
-
-        # # auth
-        # from gluon import current
-        # T = current.T
-        # self.auth_formstyle = 'divs'
-        # self.auth_photo_source = [
-        #                           (1, T("upload")),
-        #                           (2, "gravatar"),
-        #                           (3, "facebook"),
-        #                           (4, "twitter"),
-        #                           (5, T("no photo")),
-        #                          ]
-        # self.auth_gender = [
-        #                    ("Male", T("Male")),
-        #                    ("Female", T("Female")),
-        #                    ("Not specified", T("Not specified")),
-        #                    ]
-
-        # self.auth_privacy = [
-        #                     (1, T("Public")),
-        #                     (2, T("Visible only for contacts")),
-        #                     (3, T("Private")),
-        #                     ]
-        # mail
