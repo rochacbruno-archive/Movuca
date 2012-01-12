49c58d930bd32d5b37c5d4f72749808f2c15d7ae
Bruno Rocha <rochacbruno@gmail.com>
2012-01-12 04:25:47 -0200

    twittername problem

--- modules/handlers/person.py
+++ modules/handlers/person.py
@@ -318,7 +318,7 @@ class Person(Base):
         self.response.meta.description = str(user.tagline or user.about) + ' ' + str(user.city or '') + ' ' + str(user.country or '')
         self.response.meta.keywords = [user.first_name, user.last_name, user.nickname]
 
-        self.context.twittername = self.context.user.twitter.split('/')[-1] if self.context.user.twitter else None
+        self.context.twittername = self.context.user.twitter.split('/')[-1] if self.context.user.twitter else ""
 
     def account(self):
         self.context.auth = self.db.auth
