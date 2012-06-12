e23856e59771bc32db96907aafe325929dc7de03
Bruno Rocha <rochacbruno@gmail.com>
2012-06-12 19:07:51 -0300

    set language defaults to pt-br

--- models/0_language.py
+++ models/0_language.py
@@ -1,2 +1,5 @@
+if not 'siteLanguage' in request.cookies:
+    T.force('pt-br')
+
 if 'siteLanguage' in request.cookies and not (request.cookies['siteLanguage'] is None):
     T.force(request.cookies['siteLanguage'].value)
