6dc3f74d2173486e6aea7feffe2c007e71ae8ffa
Bruno Rocha <rochacbruno@gmail.com>
2012-07-31 23:53:56 -0300

    removed the default language to pt-br

--- models/0_language.py
+++ models/0_language.py
@@ -1,5 +1,5 @@
-if not 'siteLanguage' in request.cookies:
-    T.force('pt-br')
+#if not 'siteLanguage' in request.cookies:
+#    T.force('pt-br')
 
 if 'siteLanguage' in request.cookies and not (request.cookies['siteLanguage'] is None):
     T.force(request.cookies['siteLanguage'].value)
